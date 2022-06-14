d2ecef4e8805f262857a9b050e2f4db082bf72f2
pshunt <peter.s.hunt@gmail.com>
2022-06-13 19:12:30 -0700

    add CRUD functions for Room; add initial styling

--- palpro/palpro/base/views.py
+++ palpro/palpro/base/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from .models import Room
-
+from django.shortcuts import render, redirect
+from .models import Room, Topic
+from .forms import RoomForm
 
 # rooms = [
 #     {'id':1, 'name':'Lets learn Python' },
@@ -11,8 +11,13 @@ from .models import Room
 # Create your views here.
 def home(request):
     rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    
+    topics = Topic.objects.all()
+    
+    context = {'rooms': rooms, 'topics':topics}
     return render(request, 'base/home.html', context)
+
+    
 
 def room(request, pk):
     # room = None
@@ -24,4 +29,32 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
- 
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = { 'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = { 'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
